9aeda373e39dd7fed528b4485dc451af44b31b98
josipglavas <glavas.josip@icloud.com>
2023-10-07 16:25:39 +0200

    Setup basic Application and Entry point.

---
+++ GameEngine/src/Engine.h
@@ -0,0 +1,9 @@
+#pragma once
+
+//for use by GameEngine applications.
+
+#include "Engine/Application.h"
+
+// --- Entry Point ---
+#include "Engine/EntryPoint.h"
+//--------------------
---
+++ GameEngine/src/Engine/EntryPoint.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "../Engine.h"
+
+#ifdef GE_PLATFORM_WINDOWS
+
+extern Engine::Application* Engine::CreateApplication();
+
+int main(int argc, char** argv) {
+	auto app = Engine::CreateApplication();
+	app->Run();
+	delete app;
+}
+
+#endif
